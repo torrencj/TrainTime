2c49067cc3f6f05e5600ea1f208dbbc5d0edd319
John <john@Mac.local>
2017-08-23 20:49:12 -0500

    added editing delete buttons save buttons and FB key access

--- assets/traintime.js
+++ assets/traintime.js
@@ -1,4 +1,6 @@
-
+//TODO Rejigger this whole thing.
+//Set up firebase to deliver the data we need to a local object and then work with it from there.
+//Save our changes to the local object to firebase in a way that makes sense.
 
 // Initialize Firebase
 var config = {
@@ -11,8 +13,15 @@ var config = {
 };
 firebase.initializeApp(config);
 
+//count down the minutes until the train is due.
+$(document).ready(function() {
+  setInterval(update, 60000);
+  // $("#save-edits").css("display","none");
+})
+
 //creating a variable to reference the database
 var database = firebase.database();
+var userEditData = {};
 
 //is this the initial load? (Used when drawing all previous items)
 // var initial = true;
@@ -25,63 +34,176 @@ var database = firebase.database();
 // This is triggered for each item in the database on load.
 database.ref().on("child_added", function(snap) {
 // addRow by name, dest, start, freq
-addRow(snap.val().name,snap.val().dest, snap.val().start, snap.val().freq);
+// console.log(snap.val());
+console.log(snap.key);
+  addRow(snap.val().name,
+         snap.val().dest,
+         snap.val().start,
+         snap.val().freq,
+         snap.key);
 });
 
-//TODO This works.
+function update() {
+  //make all the numbers in the wait time row count down.
+  //if we're boarding, change the wait time text to the frequency and start over.
+  $("td#wait-time-live").each( function() {
+    var temp = +$(this).text(); //NaN if this is a string...
+    if (!temp) {
+      $(this).text($(this).prev().text())
+    } else {
+      if (temp > 1) {
+        $(this).text(+$(this).text() - 1);
+      } else {
+        $(this).text("Boarding")
+      }
+    }
+  });
+}
+
+
+// This works.
 // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
 //   console.log("orderByChild");
 //   console.log(snapshot.val());
 // });
 
 //Returns the difference of two dates in months, uses MomentJS
-function findMonthsWorked(inputDate) {
-  return moment().diff(moment(inputDate), 'months')
-}
+function findMinutesAway(start, freq) {
+  var diff = moment().diff(moment(start, "HH:mm A"), 'minutes'); //always pos
 
-//Returns the total value paid to the employee
-function findTotalPaid(freq, months) {
-  return freq * months;
+  if (diff > 0) {
+    return (diff % freq);
+  } else {
+    return -diff;
+  }
 }
 
 // function to create the new row
-function addRow(name, dest, start, freq) {
-  var monthsworked = findMonthsWorked(start);
+function addRow(name, dest, start, freq, key) {
+  // console.log("name: "+name+"dest: "+ dest+" start: "+ start+" freq: "+ freq);
 
-  var newRow = $("<tr>").append( $("<td>").text(name) )
-                        .append( $("<td>").text(dest) )
-                        .append( $("<td>").text(start) )
-                        .append( $("<td>").text(monthsworked) )
-                        .append( $("<td>").text(freq) )
-                        .append( $("<td>").text(findTotalPaid(freq, monthsworked)) );
+  var waitTime = findMinutesAway(start, freq);
+
+  // var newRow = $("<tr>").append( $("<td>").click(cellEdit).text(name) )
+  //                       .append( $("<td>").click(cellEdit).text(dest) )
+  //                       .append( $("<td>").click(cellEdit).text(moment().add(waitTime, 'minutes').format("LT")) )
+  //                       .append( $("<td>").click(cellEdit).text(freq) )
+  //                       .append( $("<td>").click(cellEdit).text(waitTime).attr("id","wait-time-live") );
+
+  var newRow = $("<tr>").attr("dbkey",key)
+    .append( $("<td>").attr("contenteditable","true").text(name) )
+    .append( $("<td>").attr("contenteditable","true").text(dest) )
+    .append( $("<td>").attr("contenteditable","true").text(moment().add(waitTime, 'minutes').format("LT")) )
+    .append( $("<td>").attr("contenteditable","true").text(freq) )
+    .append( $("<td>").attr("contenteditable","true").text(waitTime).attr("id","wait-time-live") )
+    // .append( $("<button>").attr("class", "btn btn-danger").attr("id", "remove-train").text("X") )
+    .append( $("<button type='button' id ='remove-train' class='close' aria-label='Close'>")
+      .html("<span aria-hidden='true'>&times;</span>")
+);
+
+  // var newRow = $("<tr>").append( $("<td>").text(name) )
+  //                       .append( $("<td>").text(dest) )
+  //                       .append( $("<td>").text(moment().add(waitTime, 'minutes').format("LT")) )
+  //                       .append( $("<td>").text(freq) )
+  //                       .append( $("<td>").text(waitTime).attr("id","wait-time-live") );
+                        // .click(cellEdit);
+  // newRow.attr("contenteditable", "true");
   $("tbody").append(newRow);
-
-
-
-
 };
 
- // submit button to push the data up to the employees table
+
+//I stole this wholesale from http://jsbin.com/satutawipo/edit TODO steal less wholesale.
+$("tbody").on("keydown", function(event) {
+  // console.log("hello there!");
+  var esc = event.which == 27,
+      newline = event.which == 13,
+      elem = event.target,
+      input = elem.nodeName != 'INPUT' && elem.nodeName != 'TEXTAREA';
+      // console.log(elem);
+
+
+  if (input) {
+    if (esc) {
+      // restore state
+      document.execCommand('undo'); //TODO This isn't exactly what I want. Not sure how to solve this.
+      elem.blur();
+    } else if (newline) {
+      // save
+      $("#save-edits").css("display","block");
+      // userEditData[$(this).] = el.innerHTML;
+      elem.blur();
+      event.preventDefault();
+    }
+  }
+});
+
+$("#save-edits").on("click", function(){
+  //TODO firebase edit
+  //TODO get the <td>'s parent key attribute and then use that to edit things on firebase.
+})
+
+ // submit button to push the data up to the train table
 $("#submit-button").on("click", function(event){
 	event.preventDefault();
 
 	var name = $("#name").val().trim();
 	var dest = $("#dest").val().trim();
-	var start = moment($("#start").val().trim());
+	var start = $("#start").val().trim();
 	var freq = $("#freq").val();
+  console.log("line 71 freq:" + freq);
 
-  console.log(start);
+  // console.log(start);
+  var initialTime = moment(start, "H");
 
-  //clear the fields
+  // clear the fields
   $("#name").val('');
 	$("#dest").val('');
 	$("#start").val('');
 	$("#freq").val('');
 
-  database.ref().push({
+  // var newKey = database.ref().push().key;
+  // console.log(newKey);
+  var trainData = {
     "name": name,
     "dest" : dest,
     "start": start,
     "freq": freq
-  })
+  };
+  var newKey = database.ref().push(trainData).key;
+  console.log(newKey);
+  // database.ref().push({
+  //   "name": name,
+  //   "dest" : dest,
+  //   "start": start,
+  //   "freq": freq
+  // })
+
+
+
+  // database.ref().update(newKey = trainData)
 });
+// var updates = {};
+// updates['/posts/' + newPostKey] = postData;
+// updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+//
+// return firebase.database().ref().update(updates);
+
+//TODO this isn't exactly what I want, but maybe I should use it instead.
+// function cellEdit() {
+//     var currentState = $(this).text();
+//     var userEdit = $("<textarea />");
+//     userEdit.val(currentState);
+//     $(this).replaceWith(userEdit);
+//     userEdit.focus();
+//     // blur the edit
+//     userEdit.blur(userEditBlurred);
+// }
+//
+// function userEditBlurred() {
+//     var current = $(this).val();
+//     var text = $("<td>");
+//     text.html(current);
+//     $(this).replaceWith(text);
+//     // setup the click event for this new div
+//     text.click(cellEdit);
+// }
